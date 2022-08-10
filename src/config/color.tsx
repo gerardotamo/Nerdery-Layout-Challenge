f73b992f216c7940719b49b6f6f933fb901bcfc1
gerardotamo <gerardotamo@ravn.co>
2022-08-10 17:58:23 -0500

    feat: add global variables to colors

---
+++ src/config/color.tsx
@@ -0,0 +1,12 @@
+
+const BaseColor = {
+    primaryColor: "#343951",
+    secondaryColor: "FF9F00",
+    whiteColor: "#FFFFFF",
+    grayColor: "#C4C4C4",
+    inactiveGrayColor: "#858A9D",
+    secondaryGrayColor: "#BEC0C8",
+
+}
+
+export {BaseColor}
