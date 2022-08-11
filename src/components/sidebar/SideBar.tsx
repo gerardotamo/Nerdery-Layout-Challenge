3c0d75bd7a9755a9ff22dc4bdb75a381c5882511
gerardotamo <gerardotamo@ravn.co>
2022-08-11 09:46:36 -0500

    feat: add styled to color palette into sideBar

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -3,7 +3,12 @@ import * as style from './syled'
 const SideBar = () => {
   return (
     <style.Content>
-      
+      <div>
+        <style.ColorPalette />
+      </div>
+      <div>
+
+      </div>
     </style.Content>
   )
 }
--- src/components/sidebar/syled.tsx
+++ src/components/sidebar/syled.tsx
@@ -9,3 +9,12 @@ export const Content = styled.div`
     flex-direction: column;
 `;
 
+export const ColorPalette = styled.div`
+    width: 74px;
+    height: 21px;
+    margin-left: 32px;
+    margin-top: 36px;
+
+    background: linear-gradient(270deg, #689EF8 0%, #FF6860 56.25%, #FF9F00 100%);
+    border-radius: 5px;    
+`;
